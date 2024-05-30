78af4e5541d50573a074e167b9f92cd3ca87e852
Geppetto095 <geppetto095@gmail.com>
2024-05-30 11:19:57 +0200

    create Controller

---
+++ src/main/java/it/lezioneone/lezione_30_5_24a/Lezione30524aApplication.java
@@ -0,0 +1,13 @@
+package it.lezioneone.lezione_30_5_24a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lezione30524aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lezione30524aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/lezioneone/lezione_30_5_24a/Lezione30524aApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lezioneone.lezione_30_5_24a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lezione30524aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
